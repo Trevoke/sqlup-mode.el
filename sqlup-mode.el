b046f7178a6350a77d5906731a560ab27ffad0cb
Aldric Giacomoni <trevoke@gmail.com>
2014-06-26 12:10:59 -0400

    Add a sqlup- prefix

--- sqlup-mode.el
+++ sqlup-mode.el
@@ -1,14 +1,39 @@
+;; sqlup.el --- Upcase SQL words for you
+
+;; Copyright 2014 Aldric Giacomoni
+
+;; Author: Aldric Giacomoni <trevoke@gmail.com>
+;; Version: 0.0.1
+;; URL: https://github.com/trevoke/sqlup.el
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (defun sqlup-insert-space-and-maybe-capitalize ()
   (interactive)
-  (maybe-capitalize-word-at-point)
+  (sqlup-maybe-capitalize-word-at-point)
   (insert " "))
 
 (defun sqlup-insert-open-parens-and-maybe-capitalize ()
   (interactive)
-  (maybe-capitalize-word-at-point)
+  (sqlup-maybe-capitalize-word-at-point)
   (insert "("))
 
-(defun maybe-capitalize-word-at-point ()
+(defun sqlup-maybe-capitalize-word-at-point ()
   (setq sqlup-current-word (thing-at-point 'symbol))
   (if (and (char-or-string-p sqlup-current-word)
            (member (intern (upcase sqlup-current-word)) sqlup-keywords))
