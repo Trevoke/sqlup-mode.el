1862da780adb082e51017679c1f54425afaaf08d
Aldric Giacomoni <trevoke@gmail.com>
2014-06-30 17:41:54 -0400

    Privatize some functions

--- sqlup-mode.el
+++ sqlup-mode.el
@@ -39,22 +39,18 @@
 ;;; Code:
 
 (defun sqlup-insert-space-and-maybe-capitalize ()
-  (interactive)
   (sqlup-maybe-capitalize-word-at-point)
   (insert " "))
 
 (defun sqlup-insert-open-parens-and-maybe-capitalize ()
-  (interactive)
   (sqlup-maybe-capitalize-word-at-point)
   (insert "("))
 
 (defun sqlup-insert-semicolon-and-maybe-capitalize ()
-  (interactive)
   (sqlup-maybe-capitalize-word-at-point)
   (insert ";"))
 
 (defun sqlup-maybe-capitalize-word-at-point ()
-  (interactive)
   (let ((sqlup-current-word (thing-at-point 'symbol))
         (sqlup-current-word-boundaries (bounds-of-thing-at-point 'symbol)))
     (if (member sqlup-current-word sqlup-keywords)
