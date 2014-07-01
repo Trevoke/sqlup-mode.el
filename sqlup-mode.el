730e879cba900a990f9a25028f2a8931b2b0e22b
Aldric Giacomoni <trevoke@gmail.com>
2014-06-30 21:07:52 -0400

    Release 0.1.3
    * Upcase all the words!

--- sqlup-mode.el
+++ sqlup-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Aldric Giacomoni <trevoke@gmail.com>
 ;; URL: https://github.com/trevoke/sqlup-mode.el
 ;; Created: Jun 25 2014
-;; Version: 0.1.2
+;; Version: 0.1.3
 ;; Keywords: sql, tools
 
 ;;; License:
